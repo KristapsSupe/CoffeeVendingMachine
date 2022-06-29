bff4fda109ee719198c4586e3a568ff46f3319a0
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-06-29 06:15:06 +0300

    Add files via upload

--- 1.php
+++ 1.php
@@ -17,6 +17,8 @@ $wallet = [
     2 => 0,
     5 => 10,
     10 => 10,
+    20 => 10,
+    50 => 10,
     100 => 10,
     200 => 10,
 ];
@@ -31,7 +33,8 @@ $drinks = [
 
 $insertedAmount = 0;
 
-while (true) {
+while (true)
+{
     foreach ($drinks as $name => $price) {
         echo "$name - $price | ";
     }
@@ -63,7 +66,6 @@ while (true) {
     if ($insertedAmount >= 120) {
         break;
     }
-
 }
 
 $chooseDrink = readline("Choose drink: ");
@@ -80,9 +82,43 @@ if ($insertedAmount < $drinks[$chooseDrink]) {
 
 $returnAmount = $insertedAmount - $drinks[$chooseDrink];
 
-echo "You choose: " . $chooseDrink . PHP_EOL;
+if ($returnAmount === 0) {
+    echo "No change" . PHP_EOL;
+}
+
+$returnCoins200 = (intdiv($returnAmount, 200));
+$balance200 = $returnAmount - ($returnCoins200 * 200);
+
+$returnCoins100 = (intdiv($balance200, 100));
+$balance100 = $balance200 - ($returnCoins100 * 100);
+
+$returnCoins50 = (intdiv($balance100, 50));
+$balance50 = $balance100 - ($returnCoins50 * 50);
+
+$returnCoins20 = (intdiv($balance50, 20));
+$balance20 = $balance50 - ($returnCoins20 * 20);
+
+$returnCoins10 = (intdiv($balance20, 10));
+$balance10 = $balance20 - ($returnCoins10 * 10);
+
+$returnCoins5 = (intdiv($balance10, 5));
+$balance5 = $balance10 - ($returnCoins5 * 5);
+
+$returnCoins2 = (intdiv($balance5, 2));
+$balance2 = $balance5 - ($returnCoins2 * 5);
+
+$returnCoins1 = (intdiv($balance2, 1));
+$balance1 = $balance2 - ($returnCoins1 * 5);
+
+
+echo "Here is your drink: " . $chooseDrink . PHP_EOL;
 echo "Your change: " . $returnAmount . PHP_EOL;
 
-
-
-
+echo "Return 200 Coins: " . $returnCoins200 . PHP_EOL;
+echo "Return 100 Coins: " . $returnCoins100 . PHP_EOL;
+echo "Return 50 Coins: " . $returnCoins50 . PHP_EOL;
+echo "Return 20 Coins: " . $returnCoins20 . PHP_EOL;
+echo "Return 10 Coins: " . $returnCoins10 . PHP_EOL;
+echo "Return 5 Coins: " . $returnCoins5 . PHP_EOL;
+echo "Return 2 Coins: " . $returnCoins2 . PHP_EOL;
+echo "Return 1 Coins: " . $returnCoins1 . PHP_EOL;
